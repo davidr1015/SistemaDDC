d70caae67f1650cfc73b6128938be006bcfb4e00
David R Delgado <dmdr1015@gmail.com>
2023-11-13 16:51:41 -0500

    Actualizacion BD

--- bd_delcampo1 (2).sql
+++ bd_delcampo1 (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-11-2023 a las 23:17:22
+-- Tiempo de generación: 13-11-2023 a las 22:50:49
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -35,6 +35,13 @@ CREATE TABLE `clientes` (
   `activo` tinyint(4) NOT NULL DEFAULT 1,
   `fecha_registro` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`cedula`, `nombre`, `correo`, `telefono`, `activo`, `fecha_registro`) VALUES
+(123458462, 'Diego Sena', 'diego@gmial,com', '2154621', 1, '2023-11-13 21:49:34');
 
 -- --------------------------------------------------------
 
@@ -85,15 +92,10 @@ CREATE TABLE `productos` (
 --
 
 INSERT INTO `productos` (`id`, `descripcion`, `precio`, `cantidad_minima`, `ubicacion`, `cantidad`, `foto`, `activo`) VALUES
-(1, 'Jamon 1000gr', 18000, 0, 'Bodega 1', 92154, '', 0),
-(2, 'Salchicha para perro x12und ', 15000, 0, 'Bodega1', 145123, '', 0),
-(3, 'Paquete Chorizos x24und 250gr', 15000, 0, 'Bodega2', 124, '', 0),
-(4, 'Carne de res', 5000, 0, 'Bodega1', 24, '', 0),
-(5, 'POIUTRW', 1900, 0, 'Bodega 3', 152, NULL, 1),
-(6, 'asdsadsad', 14999, NULL, 'Bodega 4', 1, 'prueba', 1),
-(7, 'Jamon 550gr', 2314, NULL, NULL, 15, NULL, 1),
-(8, 'Descripcion prueba', 9874, NULL, NULL, 54, NULL, 1),
-(9, 'Pechuga de Pollo 500gr', 15000, 15, NULL, 0, NULL, 1);
+(1, 'Chorizo 550gr', 14000, 10, 'Bodega principal', 0, '97c70d8c-296b-4ac3-a872-8c06d92f5ce5.jpg', 1),
+(2, 'Mortadela Tradicional 450gr', 10000, 0, 'Bodega Principal', 0, 'adb46836-3a60-43a0-aaa3-31af9567af8a.jpg', 1),
+(3, 'Salchichon Ranchero', 3000, 25, 'Bodega 3', 0, '92e9f6a5-bf23-4c47-b56a-93695a744fb1.jpg', 1),
+(4, 'Producto Descripcion', 15000, 12, 'BODEGA', 0, '6aa5788c-de3c-44f5-ad02-525ac05ccc7f.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -105,16 +107,19 @@ CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre` varchar(60) NOT NULL,
   `rol` varchar(40) NOT NULL,
+  `email` varchar(50) NOT NULL,
   `login` varchar(50) NOT NULL,
-  `pwd` varchar(10) NOT NULL
+  `pwd` varchar(10) NOT NULL,
+  `activo` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `rol`, `login`, `pwd`) VALUES
-(1, '0', '0', 'carlos', '1234');
+INSERT INTO `usuarios` (`id`, `nombre`, `rol`, `email`, `login`, `pwd`, `activo`) VALUES
+(12345678, 'Carlos Mario Tirado Tovar', 'Vendedor', 'dsa@fgdg.com', 'carlossadas', '1234', 1),
+(1003004328, 'David Delgado Medina', 'Vendedor', 'dmdr1015@hotmail.com', 'david', '1234', 1);
 
 --
 -- Índices para tablas volcadas
@@ -170,13 +175,13 @@ ALTER TABLE `detalles_compras`
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1003004329;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
