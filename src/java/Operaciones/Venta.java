3823ff9c2cff494b2d2b54616471a8fb9a063bf6
David R Delgado <dmdr1015@gmail.com>
2023-11-15 00:16:40 -0500

    Ya el carrito guarda las ventas y los detalles

--- src/java/Operaciones/Venta.java
+++ src/java/Operaciones/Venta.java
@@ -1,13 +1,15 @@
 package Operaciones;
 
+import Operaciones.*;
 import java.sql.*;
 
 public class Venta {
-         private int id;
-         private int id_usuario;
-         private int id_cliente;
-         private double total;
-         private String fecha_registro;
+
+    private int id;
+    private int id_usuario;
+    private int id_cliente;
+    private double total;
+    private String fecha_registro;
 
     public Venta(int id, int id_usuario, int id_cliente, double total, String fecha_registro) {
         this.id = id;
@@ -17,83 +19,115 @@ public class Venta {
         this.fecha_registro = fecha_registro;
     }
 
-    
-         
-     public static synchronized boolean insertar(Venta p)  throws SQLException
-   {                    
-              DBManager dbm = new DBManager();
-              Connection con = dbm.getConnection();
+    public static synchronized int insertar(Venta p) throws SQLException {
+        DBManager dbm = new DBManager();
+        Connection con = dbm.getConnection();
 
-              PreparedStatement st=con.prepareStatement("INSERT INTO productos(id,descripcion,"+
-                                                "precio,cantidad,foto,activo)"+
-                                                "VALUES(?,?,?,?,?,?)");
+// Especifica que quieres recuperar las claves generadas
+        PreparedStatement st = con.prepareStatement(
+                "INSERT INTO ventas(id_cliente, id_usuario, total) VALUES(?,?,?)",
+                PreparedStatement.RETURN_GENERATED_KEYS);
+
+        st.setInt(1, p.getId_cliente());
+        st.setInt(2, p.getId_usuario());
+        st.setDouble(3, p.getTotal());
+
+        int res = st.executeUpdate();
+        int idVentaGenerado = -1; // Inicializa con un valor que indique un fallo
+
+        if (res == 1) {
+            // Recupera las claves generadas
+            ResultSet generatedKeys = st.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                idVentaGenerado = generatedKeys.getInt(1);
+                System.out.println("ID de venta generado: " + idVentaGenerado);
+
+                // Aquí puedes hacer lo que necesites con el ID generado
+            } else {
+                System.out.println("No se pudo obtener el ID de venta generado");
+            }
+        }
+
+        st.close();
+        dbm.closeConnection(con);
+
+        return idVentaGenerado;
+    }
+
+    public static synchronized boolean insertarDetalle(int id_venta, Producto p) throws SQLException {
+        DBManager dbm = new DBManager();
+        Connection con = dbm.getConnection();
+
+        PreparedStatement st = con.prepareStatement("INSERT INTO detalles_venta(id_venta,"
+                + "id_producto, nombre, precio, cantidad, subtotal)"
+                + "VALUES(?,?,?,?,?,?)");
+        st.setInt(1, id_venta);
+        st.setInt(2, p.getId());
+        st.setString(3, p.getDescripcion());
+        st.setDouble(4, p.getPrecio());
+        st.setInt(5, p.getCantidad());
+        st.setDouble(6, p.getPrecio() * p.getCantidad());
+        int res = st.executeUpdate();
+        st.close();
+        dbm.closeConnection(con);
+        return res == 1;
+
+    }
+
+    public static synchronized boolean actualizar(Venta p) throws SQLException {
+        DBManager dbm = new DBManager();
+        Connection con = dbm.getConnection();
+
+        PreparedStatement st = con.prepareStatement("INSERT INTO tclientes(cedula,nombres,"
+                + "apellidos,direccion,email,celular)"
+                + "VALUES(?,?,?,?,?,?)");
 //             st.setString(1, p.getId());
 //             st.setString(2, p.getId_usuario());
 //             st.setString(2, p.getId_cliente());
 //             st.setString(3, p.getTotal());
 //             st.setString(4, p.getFecha_registro());
-             int res=st.executeUpdate();
-             st.close();
-             dbm.closeConnection( con );
-             return res==1;      
-   }
-     
-     public static synchronized boolean actualizar(Venta p)  throws SQLException
-   {                    
-              DBManager dbm = new DBManager();
-              Connection con = dbm.getConnection();
+        int res = st.executeUpdate();
+        st.close();
+        dbm.closeConnection(con);
+        return res == 1;
+    }
 
-              PreparedStatement st=con.prepareStatement("INSERT INTO tclientes(cedula,nombres,"+
-                                                "apellidos,direccion,email,celular)"+
-                                                "VALUES(?,?,?,?,?,?)");
+    public static synchronized boolean eliminar(Venta p) throws SQLException {
+        DBManager dbm = new DBManager();
+        Connection con = dbm.getConnection();
+
+        PreparedStatement st = con.prepareStatement("INSERT INTO tclientes(cedula,nombres,"
+                + "apellidos,direccion,email,celular)"
+                + "VALUES(?,?,?,?,?,?)");
 //             st.setString(1, p.getId());
 //             st.setString(2, p.getId_usuario());
 //             st.setString(2, p.getId_cliente());
 //             st.setString(3, p.getTotal());
 //             st.setString(4, p.getFecha_registro());
-             int res=st.executeUpdate();
-             st.close();
-             dbm.closeConnection( con );
-             return res==1;      
-   }
-     
-     public static synchronized boolean eliminar(Venta p)  throws SQLException
-   {                    
-              DBManager dbm = new DBManager();
-              Connection con = dbm.getConnection();
+        int res = st.executeUpdate();
+        st.close();
+        dbm.closeConnection(con);
+        return res == 1;
+    }
 
-              PreparedStatement st=con.prepareStatement("INSERT INTO tclientes(cedula,nombres,"+
-                                                "apellidos,direccion,email,celular)"+
-                                                "VALUES(?,?,?,?,?,?)");
+    public static synchronized boolean consultar(Venta p) throws SQLException {
+        DBManager dbm = new DBManager();
+        Connection con = dbm.getConnection();
+
+        PreparedStatement st = con.prepareStatement("INSERT INTO tclientes(cedula,nombres,"
+                + "apellidos,direccion,email,celular)"
+                + "VALUES(?,?,?,?,?,?)");
 //             st.setString(1, p.getId());
 //             st.setString(2, p.getId_usuario());
 //             st.setString(2, p.getId_cliente());
 //             st.setString(3, p.getTotal());
 //             st.setString(4, p.getFecha_registro());
-             int res=st.executeUpdate();
-             st.close();
-             dbm.closeConnection( con );
-             return res==1;      
-   }
-     
-     public static synchronized boolean consultar(Venta p)  throws SQLException
-   {                    
-              DBManager dbm = new DBManager();
-              Connection con = dbm.getConnection();
-
-              PreparedStatement st=con.prepareStatement("INSERT INTO tclientes(cedula,nombres,"+
-                                                "apellidos,direccion,email,celular)"+
-                                                "VALUES(?,?,?,?,?,?)");
-//             st.setString(1, p.getId());
-//             st.setString(2, p.getId_usuario());
-//             st.setString(2, p.getId_cliente());
-//             st.setString(3, p.getTotal());
-//             st.setString(4, p.getFecha_registro());
-             int res=st.executeUpdate();
-             st.close();
-             dbm.closeConnection( con );
-             return res==1;      
-   }
+        int res = st.executeUpdate();
+        st.close();
+        dbm.closeConnection(con);
+        return res == 1;
+    }
 
     public int getId() {
         return id;
@@ -135,7 +169,4 @@ public class Venta {
         this.fecha_registro = fecha_registro;
     }
 
-   
-    
-     
 }
--- src/java/Servlet/Servlet_peticiones.java
+++ src/java/Servlet/Servlet_peticiones.java
@@ -29,40 +29,40 @@ import javax.servlet.http.Part;
 @WebServlet(name = "Servlet_peticiones", urlPatterns = {"/Servlet_peticiones"})
 @MultipartConfig
 public class Servlet_peticiones extends HttpServlet {
-
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         response.setContentType("application/json");
         try (PrintWriter out = response.getWriter()) {
-
+            
         }
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         java.io.PrintWriter out = response.getWriter();
         try {
-
+            
             String page = request.getParameter("page");
 
 //        ****Inicio Productos****
             String editarProducto = request.getParameter("editarProducto");
             String eliminarProducto = request.getParameter("eliminarProducto");
-
+            
             if ("productos".equals(page)) {
                 Vector v = Producto.consultar();
                 request.setAttribute("productos", v);
                 request.getRequestDispatcher("/JSP/views/productos/index.jsp").forward(request, response);
             }
-
+            
             if (editarProducto != null) {
                 Vector v = Producto.consultarPorId(editarProducto);
                 request.setAttribute("producto", v);
                 request.getRequestDispatcher("/JSP/views/productos/editar.jsp").forward(request, response);
             }
-
+            
             if (eliminarProducto != null) {
                 if (Producto.eliminar(eliminarProducto)) {
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=productos");
@@ -70,17 +70,16 @@ public class Servlet_peticiones extends HttpServlet {
             }
 
 //          ****Fin Productos****
-
 //          ****Inicio Cliente****
             String editarCliente = request.getParameter("editarCliente");
             String eliminarCliente = request.getParameter("eliminarCliente");
-
+            
             if (editarCliente != null) {
                 Vector v = Cliente.consultarPorId(editarCliente);
                 request.setAttribute("cliente", v);
                 request.getRequestDispatcher("/JSP/views/clientes/editar.jsp").forward(request, response);
             }
-
+            
             if (eliminarCliente != null) {
                 if (Cliente.eliminar(eliminarCliente)) {
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=clientes");
@@ -98,7 +97,6 @@ public class Servlet_peticiones extends HttpServlet {
             }
 
 //          ****Fin Compras****
-
 //          ****Inicio Usuarios****
             String editarUsuario = request.getParameter("editarUsuario"); //Boton editarUsuario en vista usuarios/editar, recibe un id
             String eliminarUsuario = request.getParameter("eliminarUsuario"); //Boton eliminarUsuario en vista usuarios/editar, recibe un id
@@ -108,13 +106,13 @@ public class Servlet_peticiones extends HttpServlet {
                 request.setAttribute("usuarios", v);
                 request.getRequestDispatcher("/JSP/views/usuarios/index.jsp").forward(request, response);
             }
-
+            
             if (editarUsuario != null) {
                 Vector v = Usuario.consultarPorId(editarUsuario);
                 request.setAttribute("usuario", v);
                 request.getRequestDispatcher("/JSP/views/usuarios/editar.jsp").forward(request, response);
             }
-
+            
             if (eliminarUsuario != null) {
                 if (Usuario.eliminar(eliminarUsuario)) {
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=usuarios");
@@ -128,78 +126,44 @@ public class Servlet_peticiones extends HttpServlet {
                 request.setAttribute("productos", v);
                 request.getRequestDispatcher("/JSP/views/inventario/index.jsp").forward(request, response);
             }
-
+            
             if ("clientes".equals(page)) {
                 Vector v = Cliente.consultar();
                 request.setAttribute("cliente", v);
                 request.getRequestDispatcher("/JSP/views/clientes/index.jsp").forward(request, response);
             }
-
+            
             if ("compras".equals(page)) {
                 Vector v = Compra.consultar();
                 request.setAttribute("compra", v);
                 request.getRequestDispatcher("/JSP/views/compras/index.jsp").forward(request, response);
             }
-
+            
             if ("ventas".equals(page)) {
                 request.getRequestDispatcher("/JSP/views/ventas/index.jsp").forward(request, response);
             }
-
+            
             if ("caja".equals(page)) {
                 Vector v = Producto.consultar();
                 request.setAttribute("productos", v);
                 request.getRequestDispatcher("/JSP/views/caja/index.jsp").forward(request, response);
             }
-
+            
         } catch (Exception e) {
             request.setAttribute("msg", "Verifique Datos :" + e); // la e es el tipo de error
             request.setAttribute("target", "index.jsp");
             request.getRequestDispatcher("ServletError").forward(request, response);
         }
     }
-
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        
         java.io.PrintWriter out = response.getWriter();
-
-       // Leer el cuerpo de la solicitud para obtener el JSON enviado
-        BufferedReader reader = request.getReader();
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            sb.append(line);
-        }
-        String json = sb.toString();
-
-        // Analizar el JSON utilizando Gson
-        JsonParser parser = new JsonParser();
-        JsonObject jsonData = parser.parse(json).getAsJsonObject();
-        String vendedor = jsonData.get("vendedor").getAsString();
-        String cliente = jsonData.get("cliente").getAsString();
-
-        JsonArray productosArray = jsonData.getAsJsonArray("productos");
-        double totalGeneral = 0;
         
-        out.println("vendedor: " + vendedor);
-        out.println("cliente: " + cliente);
-        
-        for (int i = 0; i < productosArray.size(); i++) {
-            JsonObject productoJson = productosArray.get(i).getAsJsonObject();
-            double precio = Double.parseDouble(productoJson.get("precio").getAsString());
-            int cantidad = Integer.parseInt(productoJson.get("cantidad").getAsString());
-            double total = precio * cantidad;
-            totalGeneral += total;     
-        }
-        
-        
-
-        // Puedes enviar una respuesta al cliente si es necesario
-        response.getWriter().write("Datos de usuario recibidos correctamente");
-
         try {
-
+            
             if (request.getParameter("productos") != null) {
                 request.getRequestDispatcher("/JSP/views/usuarios/editar.jsp").forward(request, response);
             }
@@ -207,7 +171,7 @@ public class Servlet_peticiones extends HttpServlet {
 //            Productos
             String registrarProducto = request.getParameter("registrarProducto");
             String actualizarProducto = request.getParameter("actualizarProducto");
-
+            
             if (registrarProducto != null) {
                 int id = 0;
                 String descripcion = request.getParameter("descripcion");
@@ -216,15 +180,15 @@ public class Servlet_peticiones extends HttpServlet {
                 String ubicacion = request.getParameter("ubicacion");
                 int activo = 1;
                 String newFileName;
-
+                
                 Part filePart = request.getPart("foto");
                 newFileName = loadFile(filePart);
-
+                
                 if (Producto.insertar(new Producto(id, descripcion, precio, cantidad_minima, ubicacion, newFileName))) {
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=productos");
                 }
             }
-
+            
             String productos = request.getParameter("productos");
             if (productos != null) {
                 Vector v = Producto.consultarPorId("1");
@@ -248,7 +212,7 @@ public class Servlet_peticiones extends HttpServlet {
 
 //          Clientes
             String registrarCliente = request.getParameter("registrarCliente");
-
+            
             if (registrarCliente != null) {
                 String cedula = request.getParameter("cedula");
                 String nombre = request.getParameter("nombre");
@@ -256,13 +220,13 @@ public class Servlet_peticiones extends HttpServlet {
                 String telefono = request.getParameter("telefono");
                 String fecha_registro = request.getParameter("fecha_registro");
                 String activo = request.getParameter("activo");
-
+                
                 if (Cliente.insertar(new Cliente(cedula, nombre, correo, telefono, activo, fecha_registro))) {
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=clientes");
                 }
-
+                
             }
-
+            
             if (actualizarProducto != null) {
                 int id = Integer.parseInt(request.getParameter("id"));
                 String descripcion = request.getParameter("descripcion");
@@ -272,25 +236,25 @@ public class Servlet_peticiones extends HttpServlet {
                 String foto_actual = request.getParameter("foto_actual");
                 int activo = 1;
                 String newFileName;
-
+                
                 Part filePart = request.getPart("foto");
                 newFileName = loadFile(filePart);
-
+                
                 if ("".equals(newFileName)) {
                     newFileName = foto_actual;
                 }
-
+                
                 if (Producto.actualizar(new Producto(id, descripcion, precio, cantidad_minima, ubicacion, newFileName))) {
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=productos");
                 }
-
+                
             }
 
 //            Fin Productos
 //            Usuarios
             String registrarUsuario = request.getParameter("registrarUsuario");
             String actualizarUsuario = request.getParameter("actualizarUsuario");
-
+            
             if (registrarUsuario != null) {
                 int id = Integer.parseInt(request.getParameter("cedula"));
                 String nombres = request.getParameter("nombres");
@@ -298,13 +262,13 @@ public class Servlet_peticiones extends HttpServlet {
                 String rol = request.getParameter("rol");
                 String login = request.getParameter("username");
                 String pwd = request.getParameter("contraseña");
-
+                
                 if (Usuario.insertar(new Usuario(id, nombres, rol, email, login, pwd))) {
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=usuarios");
                 }
-
+                
             }
-
+            
             if (actualizarUsuario != null) {
                 int id = Integer.parseInt(request.getParameter("cedula"));
                 String nombres = request.getParameter("nombres");
@@ -312,15 +276,15 @@ public class Servlet_peticiones extends HttpServlet {
                 String rol = request.getParameter("rol");
                 String login = request.getParameter("username");
                 String contraseña_actual = request.getParameter("contraseña_actual");
-
+                
                 out.print("actualziar");
-
+                
                 if (Usuario.actualizar(new Usuario(id, nombres, rol, email, login, contraseña_actual))) {
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=usuarios");
                 }
-
+                
             }
-            
+
 //            Actualizar Cliente
             String actualizarCliente = request.getParameter("actualizarCliente");
             if (actualizarCliente != null) {
@@ -331,26 +295,74 @@ public class Servlet_peticiones extends HttpServlet {
                 String activo = request.getParameter("activo");
                 String fecha_registro = request.getParameter("fecha_registro");
                 
-                
-
                 if (Cliente.actualizar(new Cliente(cedula, nombre, correo, telefono, activo, fecha_registro))) {
                     out.print("actualziar");
                     response.sendRedirect("/SistemaDDC/Servlet_peticiones?page=clientes");
                 }
-
+                
             }
+
+            // Leer el cuerpo de la solicitud para obtener el JSON enviado
+            BufferedReader reader = request.getReader();
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            String json = sb.toString();
+
+            // Analizar el JSON utilizando Gson
+            JsonParser parser = new JsonParser();
+            JsonObject jsonData = parser.parse(json).getAsJsonObject();
+            int vendedor = Integer.parseInt(jsonData.get("vendedor").getAsString());
+            int cliente = Integer.parseInt(jsonData.get("cliente").getAsString());
+            
+            JsonArray productosArray = jsonData.getAsJsonArray("productos");
+            double totalGeneral = 0;
+            
+            out.println("vendedor: " + vendedor);
+            out.println("cliente: " + cliente);
+            
+            for (int i = 0; i < productosArray.size(); i++) {
+                JsonObject productoJson = productosArray.get(i).getAsJsonObject();
+                double precio = Double.parseDouble(productoJson.get("precio").getAsString());
+                int cantidad = Integer.parseInt(productoJson.get("cantidad").getAsString());
+                double total = precio * cantidad;
+                totalGeneral += total;                
+                
+                out.print(totalGeneral);
+            }
+            
+            int id_venta = Venta.insertar(new Venta(0, cliente, vendedor, totalGeneral, ""));
+            if ( id_venta != 0) {
+                for (int i = 0; i < productosArray.size(); i++) {
+                JsonObject productoJson = productosArray.get(i).getAsJsonObject();
+                int id = Integer.parseInt(productoJson.get("producto").getAsString());
+                String descripcion = productoJson.get("nombre").getAsString();
+                Double precio = Double.parseDouble(productoJson.get("precio").getAsString());
+                int cantidad = Integer.parseInt(productoJson.get("cantidad").getAsString());
+                
+                Venta.insertarDetalle(id_venta, new Producto(id, descripcion, precio, cantidad));
+            }
+            }else{
+                response.getWriter().write("Datos de usuario recibidos correctamente");
+            }
+//        
+//
+            // Puedes enviar una respuesta al cliente si es necesario
+            
         } catch (Exception e) {
             request.setAttribute("msg", "Verifique Datos :" + e); // la e es el tipo de error
             request.setAttribute("target", "index.jsp");
 //            request.getRequestDispatcher("ServletError").forward(request, response);
         }
     }
-
+    
     @Override
     public String getServletInfo() {
         return "Short description";
     }
-
+    
     public String loadFile(Part filePart) throws IOException {
         String originalFileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
         String newFileName = "";
@@ -375,10 +387,10 @@ public class Servlet_peticiones extends HttpServlet {
             try (InputStream input = filePart.getInputStream()) {
                 Files.copy(input, Paths.get(newFilePath), StandardCopyOption.REPLACE_EXISTING);
             }
-
+            
         }
-
+        
         return newFileName;
     }
-
+    
 }
